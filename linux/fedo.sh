110a4038b9a0d3d9ab5e4051435698fa25992ef4
solo-ninja <77336396+solo-ninja@users.noreply.github.com>
2021-01-14 20:54:22 +0630

    Update fedo.sh

--- linux/fedo.sh
+++ linux/fedo.sh
@@ -1,4 +1,2 @@
-pkg install wget 
-wget https://raw.githubusercontent.com/nmilosev/termux-fedora/master/termux-fedora.sh
-chmod 777 termux-fedora.sh
-bash termux-fedora.sh
+pkg install wget -y && /data/data/com.termux/files/usr/bin/wget https://raw.githubusercontent.com/nmilosev/termux-fedora/master/termux-fedora.sh
+sh termux-fedora.sh f33_arm64
